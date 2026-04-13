0e8a0c374a5222bb1f134e53e89252f2179f5bbe
Seiytri <183451351+Seiyana@users.noreply.github.com>
2026-04-14 02:57:33 +0800

    mp-routes and db initialization

--- kalookonek_backend/mp/urls.py
+++ kalookonek_backend/mp/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('mp/dashboard/', views.dashboard, name='dashboard'),
+    path('mp/update/', views.mp, name='mp'),
+    path('mp/patients/', views.patient_directory, name='patient_directory'),
+    path('mp/patients/search/', views.search_patient_by_name, name='search_patient_by_name'),
+    path('mp/patients/filter/', views.search_filter_barangay, name='search_filter_barangay'),
+    path('mp/patients/<int:patient_id>/record/', views.patient_record, name='patient_record'),
+    path('mp/schedule/', views.schedule, name='mp_schedule'),
+    path('mp/schedule/history/', views.schedule_history, name='mp_schedule_history'),
 ]
--- kalookonek_backend/mp/views.py
+++ kalookonek_backend/mp/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+def dashboard(request):
+    if request.method == 'GET':
+        #pass
+         return HttpResponse("mp dashboard ok")
+
+def mp(request):
+    if request.method == 'GET':
+        pass
+        #return HttpResponse("mp profile ok")
+    elif request.method == 'UPDATE':
+        pass
+        #return HttpResponse("mp profile updated")
+
+def patient_directory(request): 
+    if request.method == 'GET':
+        pass
+        #return HttpResponse("mp patient directory ok")
+
+def search_patient_by_name(request):
+    if request.method == 'GET':
+        pass
+        #return HttpResponse("mp search patient by name ok")
+
+def search_filter_barangay(request):
+    if request.method == 'GET':
+        pass
+        #return HttpResponse("mp search filter barangay ok")
+
+def patient_record(request, patient_id):
+    if request.method == 'GET':
+        pass
+        #return HttpResponse("mp patient record ok")
+    elif request.method == 'UPDATE':
+        pass
+        #return HttpResponse("mp patient record updated")
+
+def schedule(request):
+    if request.method == 'GET':
+        pass
+        #return HttpResponse("mp schedule ok")
+
+def schedule_history(request):
+    if request.method == 'GET':
+        pass
+        #return HttpResponse("mp schedule history ok")
