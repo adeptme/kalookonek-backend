3d69f5f96f0d3e986fe052d210d2f55bcf05c28c
Sean Lucino <sean.lucino1@gmail.com>
2026-04-25 12:35:01 +0800

    cors csrf update

--- kalookonek_backend/core/settings.py
+++ kalookonek_backend/core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
     #local
     'kalookonek_backend.accounts',
     'kalookonek_backend.mp',
@@ -51,11 +52,20 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    os.environ.get('CORS_ALLOWED_ORIGIN')
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    os.environ.get('CORS_ALLOWED_ORIGIN')
 ]
 
 ROOT_URLCONF = 'kalookonek_backend.core.urls'
