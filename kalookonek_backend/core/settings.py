795a9558706fbaf7c5d98dfe440f3130ff5b7960
emikss <espiriturafaelll@gmail.com>
2026-05-14 19:24:49 +0800

    almost all features are integrated

--- kalookonek_backend/core/settings.py
+++ kalookonek_backend/core/settings.py
@@ -67,6 +67,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
 ]
 
+CORS_ALLOW_CREDENTIALS = True  
+CSRF_TRUSTED_ORIGINS = ["http://localhost:5173"]
+
 CORS_ALLOW_HEADERS = [
     "accept",
     "authorization",
